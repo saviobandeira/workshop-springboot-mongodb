2b6a9fbf09f55d4025dafbab4ac626478add7244
Sávio Bandeira <savio-bandeira@hotmail.com>
2025-08-21 22:32:01 -0300

    Operacao de instanciacao da base de dados

---
+++ src/main/java/com/saviobandeira/workshopmongo/config/Instantiation.java
@@ -0,0 +1,29 @@
+package com.saviobandeira.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+import com.saviobandeira.workshopmongo.domain.User;
+import com.saviobandeira.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args0) throws Exception {
+
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
